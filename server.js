0d08bd8baf87e3ed5f02e663ee819e7249627550
mufaqar <mufaqar@gmail.com>
2024-02-28 20:00:28 +0500

    done

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-import express from 'express'
-import colors from "colors"
-import dotenv from 'dotenv'
-import morgan from 'morgan'
-import ConnectDB from './config/db.js'
-import router from './Routes/authroutes.js'
-import cors from 'cors'
-import categoryroutes from './Routes/categoryroutes.js'
-import productroutes from './Routes/productroutes.js'
+import express from 'express';
+import colors from 'colors';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import ConnectDB from './config/db.js';
+import router from './Routes/authroutes.js';
+import cors from 'cors';
+import categoryroutes from './Routes/categoryroutes.js';
+import productroutes from './Routes/productroutes.js';
 // .env config
 
 dotenv.config();
@@ -15,29 +15,36 @@ dotenv.config();
 
 ConnectDB();
 
-// rest object 
+// rest object
 
-const app = express()
+const app = express();
 
 // middlewares
-app.use(cors())
-app.use(express.json())
-app.use(morgan("dev"))
+app.use(
+  cors({
+    origin: 'https://express12.vercel.app/',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(morgan('dev'));
 
 // Routes
 
-app.use('/api/vi/auth',router)
-app.use('/api/vi/category',categoryroutes)
-app.use('/api/vi/product',productroutes)
+app.use('/api/vi/auth', router);
+app.use('/api/vi/category', categoryroutes);
+app.use('/api/vi/product', productroutes);
 
 app.get('/', (req, res) => {
-    res.send("<h1>Welcome To Mern Stack Project</h1>")
-})
+  res.send('<h1>Welcome To Mern Stack Project</h1>');
+});
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-    console.log(`server running on ${process.env.MODE} mode on port ${PORT}`.bgCyan.white)
-})
-
+  console.log(
+    `server running on ${process.env.MODE} mode on port ${PORT}`.bgCyan.white
+  );
+});
 
 // nodemon running command  {npm run server}
