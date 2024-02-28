68d3f561dd0577e6bfb2256a4ad7a1de6b095f68
mufaqar <mufaqar@gmail.com>
2024-02-28 21:48:53 +0500

    updated

--- api/index.js
+++ api/index.js
@@ -4,26 +4,26 @@ import express from 'express';
 const app = express();
 const port = 5000
 
-// // middlewares
-// app.use(
-//   cors({
-//     origin: ["https://express12.vercel.app"],
-//     methods: ["GET","PUT","POST","DELETE"],
-//     credentials: true,
-//   })
-// );
-// app.use(express.json());
+// middlewares
+app.use(
+  cors({
+    origin: ["https://express12.vercel.app"],
+    methods: ["GET","PUT","POST","DELETE"],
+    credentials: true,
+  })
+);
+app.use(express.json());
 
-//ConnectDB();
+ConnectDB();
 
-//mongoose.connect("mongodb+srv://rabbaniusama1:dobaranhi2@cluster0.2bb7j0k.mongodb.net/mernstack");
-//app.use(morgan('dev'));
+mongoose.connect("mongodb+srv://rabbaniusama1:dobaranhi2@cluster0.2bb7j0k.mongodb.net/mernstack");
+app.use(morgan('dev'));
 
-// Routes
+Routes
 
-// app.use('/api/vi/auth', router);
-// app.use('/api/vi/category', categoryroutes);
-// app.use('/api/vi/product', productroutes);
+app.use('/api/vi/auth', router);
+app.use('/api/vi/category', categoryroutes);
+app.use('/api/vi/product', productroutes);
 
 app.get('/', (req, res) => {
   res.send('<h1>Welcome To Mern Stack Project 5000</h1>');
